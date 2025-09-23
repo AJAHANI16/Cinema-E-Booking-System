8b1d50ad19befa56612b19a574078d2054d55126
Amir Cox <amircoxx31@gmail.com>
2025-09-23 13:06:26 -0400

    configured ts and tested it

---
+++ cinemaProject/cinema-e-Booking/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+function Navbar() {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto">
+        <ul className="flex space-x-4">
+          <li>
+            <h2 className="text-white text-lg font-semibold">Test1</h2>
+          </li>
+          <li>
+            <h2 className="text-white text-lg font-semibold">Test2</h2>
+          </li>
+          <li>
+            <h2 className="text-white text-lg font-semibold">Test3</h2>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
